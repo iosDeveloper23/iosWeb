9d44fc7f2379dc2f3410b8e47e058700950dc564
Farrukh Khamidov <farrukh.khamidov>
2025-09-15 22:39:37 +0500

    Initial Commit

---
+++ WebProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebProject
+//
+//  Created by Farrukh Khamidov on 15/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebProject/WebProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebProjectApp.swift
+//  WebProject
+//
+//  Created by Farrukh Khamidov on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WebProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
